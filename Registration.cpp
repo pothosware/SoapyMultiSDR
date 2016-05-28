47cda54aa2eaa416bf1a10fbd4cad721b260d42b
Josh Blum <josh@joshknows.com>
2016-05-27 18:17:38 -0700

    soapy multi sdr boilerplate

---
+++ Registration.cpp
@@ -0,0 +1,32 @@
+// Copyright (c) 2016-2016 Josh Blum
+// SPDX-License-Identifier: BSL-1.0
+
+#include "SoapyMultiSDR.hpp"
+#include <SoapySDR/Registry.hpp>
+
+/***********************************************************************
+ * Discovery routine -- find acceptable multi-devices
+ * Because single devices instances will be discoverable normally
+ * this routine should only yield results when specifically invoked.
+ **********************************************************************/
+static std::vector<SoapySDR::Kwargs> findMultiSDR(const SoapySDR::Kwargs &args)
+{
+    std::vector<SoapySDR::Kwargs> result;
+
+    
+
+    return result;
+}
+
+/***********************************************************************
+ * Factory routine -- create a device with multiple internal handles.
+ **********************************************************************/
+static SoapySDR::Device *makeMultiSDR(const SoapySDR::Kwargs &args)
+{
+    
+}
+
+/***********************************************************************
+ * Registration
+ **********************************************************************/
+static SoapySDR::Registry registerRemote("multi", &findMultiSDR, &makeMultiSDR, SOAPY_SDR_ABI_VERSION);
---
+++ SoapyMultiSDR.hpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2016-2016 Josh Blum
+// SPDX-License-Identifier: BSL-1.0
+
+#pragma once
+#include <SoapySDR/Device.hpp>
