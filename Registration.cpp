96a68aadf6a9b9f90b1e2542ce957228d39b5d83
Josh Blum <josh@joshknows.com>
2016-06-04 23:19:54 -0700

    work on find implementation

--- Registration.cpp
+++ Registration.cpp
@@ -4,6 +4,58 @@
 #include "SoapyMultiSDR.hpp"
 #include <SoapySDR/Registry.hpp>
 
+//! Use this magic stop key in the server to prevent infinite loops
+#define SOAPY_MULTI_KWARG_STOP "soapy_multi_no_deeper"
+
+//! Use this key prefix to pass in args that will become local
+#define SOAPY_MULTI_KWARG_PREFIX "multi:"
+
+/***********************************************************************
+ * Args translator for nested keywords
+ **********************************************************************/
+static SoapySDR::Kwargs translateArgs(const SoapySDR::Kwargs &args, const size_t index)
+{
+    SoapySDR::Kwargs argsOut;
+
+    //stop infinite loops with special keyword
+    argsOut[SOAPY_MULTI_KWARG_STOP] = "";
+
+    //copy all non-multi keys
+    for (auto &pair : args)
+    {
+        if (isIndexedName(pair.first)) continue;
+        if (pair.first == "driver") continue; //don't propagate local driver filter
+        if (pair.first == "type") continue; //don't propagate local sub-type filter
+        if (pair.first.find(SOAPY_MULTI_KWARG_PREFIX) == std::string::npos)
+        {
+            argsOut[pair.first] = pair.second;
+        }
+    }
+
+    //write all multi keys with prefix stripped
+    for (auto &pair : args)
+    {
+        if (pair.first.find(SOAPY_MULTI_KWARG_PREFIX) == 0)
+        {
+            static const size_t offset = std::string(SOAPY_MULTI_KWARG_PREFIX).size();
+            argsOut[pair.first.substr(offset)] = pair.second;
+        }
+    }
+
+    //when indexed, copy in by stripped key when index matches
+    //drop when the index does not match current index
+    for (auto &pair : args)
+    {
+        if (not isIndexedName(pair.first)) continue;
+
+        size_t splitIndex = 0;
+        const auto name = splitIndexedName(pair.first, splitIndex);
+        if (splitIndex == index) argsOut[name] = pair.second;
+    }
+
+    return argsOut;
+}
+
 /***********************************************************************
  * Discovery routine -- find acceptable multi-devices
  * Because single devices instances will be discoverable normally
@@ -26,9 +78,34 @@ static std::vector<SoapySDR::Kwargs> findMultiSDR(const SoapySDR::Kwargs &args)
     if (maxIndex < 0) return result;
 
     //gather results at a specific device index
+    //TODO handle multiple results
+    SoapySDR::Kwargs result0;
     for (size_t index = 0; index <= size_t(maxIndex); index++)
     {
-        
+        const auto args_i = translateArgs(args, index);
+        const auto results_i = SoapySDR::Device::enumerate(args_i);
+        if (results_i.empty()) return result; //nothing for this index
+        for (const auto &resultArgs : results_i.front())
+        {
+            result0[toIndexedName(resultArgs.first, index)] = resultArgs.second;
+        }
+    }
+    result.push_back(result0);
+
+    //remove instances of the stop key from the result
+    for (auto &resultArgs : result)
+    {
+        resultArgs.erase(SOAPY_MULTI_KWARG_STOP);
+        if (resultArgs.count("driver") != 0)
+        {
+            resultArgs["multi:driver"] = resultArgs.at("driver");
+            resultArgs.erase("driver");
+        }
+        if (resultArgs.count("type") != 0)
+        {
+            resultArgs["multi:type"] = resultArgs.at("type");
+            resultArgs.erase("type");
+        }
     }
 
     return result;
